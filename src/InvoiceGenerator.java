9043f9ab2373715942c352fc105ea1ea7714f384
KunigiMohan <kmohanr5@gmail.com>
2021-11-30 14:47:27 +0530

    Added : Method to get invoice of given Userid.

--- src/InvoiceGenerator.java
+++ src/InvoiceGenerator.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class InvoiceGenerator {
 /*
 * caluculateFare() to to caluculate fare of cab
@@ -25,4 +28,29 @@ public class InvoiceGenerator {
         }
         return new InvoiceSummary(ride1.length, total_Fare);
     }
+
+/*
+* getInvoice() return Invoice summary of given user id
+* */
+
+    public static InvoiceSummary getInvoice(int userId) {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        Map<Integer, Ride[]> map = new HashMap<>();
+        Ride[] rides = {new Ride(2.0, 5), new Ride(0.1, 1)};
+        Ride[] ride1 = {new Ride(2.0, 5), new Ride(0.1, 1)};
+
+        map.put(3, rides);
+        map.put(1, ride1);
+
+        for (Map.Entry<Integer, Ride[]> entry : map.entrySet())
+        {
+            if (userId == entry.getKey())
+            {
+                Ride[] ridesArray = entry.getValue();
+                return invoiceGenerator.caluculateFare(ridesArray);
+            }
+        }
+        return null;
+    }
+
 }
